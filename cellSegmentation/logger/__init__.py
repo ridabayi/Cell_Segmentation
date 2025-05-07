5c5aa59d41fd8a9d5d23d920a4cbe30360f75bfa
ridabayi <bayi.rida@gmail.com>
2025-05-07 19:53:36 +0200

    pipeline

--- cellSegmentation/logger/__init__.py
+++ cellSegmentation/logger/__init__.py
@@ -6,7 +6,7 @@ from from_root import from_root
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(from_root(), 'log', LOG_FILE)
-os.mkdir(log_path, exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
 lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name = 'cellSegmentation',
     version= '0.0.0',
-    author= 'Rida BAYI ',
-    author_email= 'bayi.rida@gmail.com',
+    author= 'Boktiar Ahmed Bappy',
+    author_email= 'entbappy73@gmail.com',
     packages= find_packages(),
     install_requires = []
-    )
+
+)
